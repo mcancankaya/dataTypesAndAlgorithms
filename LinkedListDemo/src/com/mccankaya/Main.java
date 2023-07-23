78486cfdd5a6c432a0535d64160079c6ca99c780
mcancankaya <mccankaya45@hotmail.com>
2023-07-23 22:30:34 +0300

    Linked list add, delete methods

--- LinkedListDemo/src/com/mccankaya/Main.java
+++ LinkedListDemo/src/com/mccankaya/Main.java
@@ -1,16 +1,61 @@
 package com.mccankaya;
 
+import java.util.Scanner;
+
 import com.mccankaya.linkedList.LinkedList;
 
 public class Main{
 	
 	public static void main(String[] args) {
-		LinkedList<String> list = new LinkedList<String>();
-		list.addFirst("1.");
-		list.addFirst("2.");
-		list.addFirst("3.");
-		list.addLast("son1");
-		list.addLast("Son2");
-		list.printList();
+		stringTest();
 	}
+	
+	public static void stringTest() {
+		Scanner scanner = new Scanner(System.in);
+		LinkedList<String> list = new LinkedList<String>();
+		boolean status = true;
+		int action;
+		System.out.println("---- Linked List Demo ----");
+		do {
+			System.out.println("\n1. Başa ekle\n2. Sona ekle\n3. Araya ekle\n4. Listeyi yazdır\n5. Baştan Sil\n6. Sondan Sil\n7. Aradan Sil");
+			System.out.print("Lütfen bir işlem seçin : ");
+			action = scanner.nextInt();
+			
+			switch (action) {
+			case 1:
+				System.out.print("Başa eklemek istediğin elemanı gir : ");
+				list.addFirst(scanner.next());
+				break;
+			case 2:
+				System.out.print("Sona eklemek istediğin elemanı gir : ");
+				list.addLast(scanner.next());
+				break;
+			case 3:
+				int index;
+				System.out.print("Eklenecek indis numarasını gir : ");
+				index = scanner.nextInt();
+				System.out.print(index + ". indise eklemek istediğin elemanı gir : ");
+				list.addIndex(index, scanner.next());
+				break;
+			case 4:
+				list.printList();
+				break;
+			case 5:
+				list.removeFirst();
+				break;
+			case 6:
+				list.removeLast();
+				break;
+			case 7:
+				System.out.print("Silmek istediğin indisi gir : ");
+				list.removeIndex(scanner.nextInt());
+				break;
+			default:
+				System.out.println("Hatalı Seçim.");;
+			}
+		
+		}while(status);
+		
+	}
+	
 }
--- LinkedListDemo/src/com/mccankaya/linkedList/LinkedList.java
+++ LinkedListDemo/src/com/mccankaya/linkedList/LinkedList.java
@@ -1,76 +1,134 @@
 package com.mccankaya.linkedList;
+
+import java.util.Iterator;
+
 /**
  * @author can
  */
 public class LinkedList<T> {
 	private Node<T> head;
 	private Node<T> tail;
-	
+
 	public LinkedList() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
+
 	public void addFirst(T data) {
 		Node<T> addNode = new Node<T>(data);
 		Node<T> temp = head;
-		
+
 		if (checkHeadIsNull()) {
 			head = addNode;
 			tail = addNode;
-		}else {
+		} else {
 			addNode.setNext(temp);
 			head = addNode;
 		}
 	}
-	
+
 	public void addLast(T data) {
 		Node<T> addNode = new Node<T>(data);
 		Node<T> temp = tail;
 		if (checkHeadIsNull()) {
 			addFirst(data);
-		}else {
+		} else {
 			temp.setNext(addNode);
 			tail = addNode;
 		}
 	}
-	
+
+	public void addIndex(int index, T data) {
+
+		if (checkHeadIsNull() || index <= 0) {
+			addFirst(data);
+		} else if (index > getLength()) {
+			addLast(data);
+
+		} else {
+			Node<T> tempPrev = head;
+			Node<T> temp = head;
+			Node<T> addNode = new Node<T>(data);
+
+			for (int i = 0; i < index; i++) {
+				tempPrev = temp;
+				temp = temp.getNext();
+			}
+			tempPrev.setNext(addNode);
+			addNode.setNext(temp);
+		}
+	}
+
+	public void removeFirst() {
+		if (!checkHeadIsNull()) {
+			head = head.getNext();
+		}
+	}
+
+	public void removeLast() {
+		if (tail != null) {
+			Node<T> tempPrev = head;
+			Node<T> temp = head;
+			while (temp.getNext() != null) {
+				tempPrev = temp;
+				temp = temp.getNext();
+			}
+			temp = tempPrev;
+			tempPrev.setNext(null);
+			tail = temp;
+
+		}
+	}
+
+	public void removeIndex(int index) {
+		Node<T> tempPrev = head;
+		Node<T> temp = head;
+		int i = 0;
+		while (i<index) {
+			
+			tempPrev = temp;
+			temp = temp.getNext();
+			i++;
+		}
+		tempPrev.setNext(temp.getNext());
+		
+		
+	}
+
 	public void printList() {
 		Node<T> temp = head;
-		while(temp != null) {
-			System.out.print(temp.getData()+" -> ");
+		while (temp != null) {
+			System.out.print(temp.getData() + " -> ");
 			temp = temp.getNext();
 		}
 	}
-	
-	
+
 	private boolean checkHeadIsNull() {
-		return head==null?true:false;
+		return head == null ? true : false;
 	}
-	
-	private int getLength() {
+
+	public int getLength() {
 		Node<T> temp = head;
 		int count = 0;
-		while(temp != null) {
+		while (temp != null) {
 			count++;
 			temp = temp.getNext();
 		}
+
 		return count;
 	}
-	
-	
-	public Node<T> getFirst(){
+
+	public Node<T> getFirst() {
 		return head;
 	}
-	
+
 	public void setFirst(Node<T> head) {
 		this.head = head;
 	}
-	
-	public Node<T> getLast(){
+
+	public Node<T> getLast() {
 		return tail;
 	}
-	
+
 	public void setLast(Node<T> tail) {
 		this.tail = tail;
 	}
